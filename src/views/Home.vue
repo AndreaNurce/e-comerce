e81ee70d6bb5bbaf64166cea3d45119d6ebc2338
AndreaNurce <andrea-dibra@hotmail.com>
2021-04-07 20:23:02 +0200

    adding hedaer components

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="Home" >
+        <nav-bar/>
+        <nav-bar-mobile/>
+    </section>
+</template>
+
+<script>
+import navBar from '@/components/navBar.vue'
+import navBarMobile from '@/components/navBarMobile.vue'
+export default {
+    components:{
+        navBar,
+        navBarMobile,
+    }
+
+}
+</script>
+
+<style>
+
+</style>
