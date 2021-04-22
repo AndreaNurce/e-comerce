786baaa0d2a9d3d68ef495bd67761032ad4bd843
AndreaNurce <53390635+AndreaNurce@users.noreply.github.com>
2021-04-22 11:15:49 +0200

    syling ADmin dashboard

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,38 +1,64 @@
 <template>
-    <section>
-        <div class="menu">
-            <ul>
-                <li @click="editor = 'Landing'">Landing Page</li>
-                <li @click="editor = 'Collections'">Collections</li>
-                <li @click="editor = 'Products'">Products</li>
-            </ul>
-        </div>
-        <div class="handler-wrapper" >
-            <collections v-if="editor =='Landing'"/>
-            <landing v-if="editor =='Collections'"/>
-            <products v-if="editor =='Products'" />
-        </div>
-    </section>
+  <section>
+    <div class="menu">
+      <ul>
+        <li @click="editor = 'Landing'">Landing Page</li>
+        <li @click="editor = 'Collections'">Collections</li>
+        <li @click="editor = 'Products'">Products</li>
+      </ul>
+    </div>
+    <div class="handler-wrapper">
+      <collections v-if="editor == 'Landing'" />
+      <landing v-if="editor == 'Collections'" />
+      <products v-if="editor == 'Products'" />
+    </div>
+  </section>
 </template>
 
 <script>
-import collections from '@/components/admin/collections.vue'
-import landing from '@/components/admin/landing.vue'
-import products from '@/components/admin/products.vue'
+import collections from "@/components/admin/collections.vue";
+import landing from "@/components/admin/landing.vue";
+import products from "@/components/admin/products.vue";
 export default {
-    data() {
-        return {
-            editor : null
-        }
-    },components:{
-        collections,
-        landing,
-        products,
-    }
-
-}
+  data() {
+    return {
+      editor: null,
+    };
+  },
+  components: {
+    collections,
+    landing,
+    products,
+  },
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+.menu {
+min-width:200px;
+  width: 20%;
+  height: calc(100vh - 80px);
+  display: flex;
+  justify-content: center;
+  ul {
+        display: flex;
+  align-items: center;
+  flex-direction: column;
+    padding: 10px;
 
-</style>
+    li {
+      cursor: pointer;
+      padding: 8px;
+      list-style: none;
+      font-family:Verdana, Geneva, Tahoma, sans-serif;
+      overflow: hidden;
+    width: 90%;
+
+    }
+  }
+}
+
+.handler-wrapper {
+  width: 80%;
+}
+</style>>
