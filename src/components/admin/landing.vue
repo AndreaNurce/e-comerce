b6031d866c192f62eac68917dc4d94b3e5a91db9
AndreaNurce <53390635+AndreaNurce@users.noreply.github.com>
2021-04-27 12:07:33 +0200

    converting from binary to base64 image and serving image

--- src/components/admin/landing.vue
+++ src/components/admin/landing.vue
@@ -3,38 +3,47 @@
     <form @submit.prevent="datas()">
       <input v-model="tittle" type="text" name="tittle" />
       <input v-model="subTittle" type="text" name="subTittle" />
-      <input ref="file" type="file" name="image"/>
+      <input ref="file" type="file" name="image" />
       <button>Update</button>
     </form>
   </div>
+  <img :src="image" alt="">
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
   data() {
     return {
       tittle: "",
       subTittle: "",
+      image: "",
     };
   },
   methods: {
-    datas() {
+    async datas() {
       let fd = new FormData();
 
       fd.append("tittle", this.tittle);
       fd.append("subTittle", this.subTittle);
       fd.append("image", this.$refs.file.files[0]);
-      axios
-        .post("http://localhost:8081/test-files", fd)
-        .then(function () {
-          console.log("SUCCESS!!");
-        })
-        .catch(function () {
-          console.log("FAILURE!!");
-        });
+      await axios.post("http://localhost:8081/test-files", fd,{
+         headers: {
+      'Content-Type': 'multipart/form-data'
+    }
+      });
     },
-  },
+  },created(){
+  axios.get("http://localhost:8081/test-files").then(function(res)  {
+    console.log(res.data.items[0].img.data.data);
+
+    let base64String = btoa(
+            String.fromCharCode.apply(null, new Uint8Array(res.data.items[0].img.data.data))
+          );
+   this.image =  "data:image/jpg;base64," + base64String;
+    
+    }.bind(this));
+  }
 };
 </script>
 
