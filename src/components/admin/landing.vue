2e70dd1ee10d0b6e7c3cffe5892717552915bba0
AndreaNurce <53390635+AndreaNurce@users.noreply.github.com>
2021-04-28 14:41:30 +0200

    making admin/landing page look better

--- src/components/admin/landing.vue
+++ src/components/admin/landing.vue
@@ -1,52 +1,109 @@
 <template>
   <div class="landing-form">
-    <form @submit.prevent="datas()">
-      <input v-model="tittle" type="text" name="tittle" />
-      <input v-model="subTittle" type="text" name="subTittle" />
-      <input ref="file" type="file" name="image" />
-      <button>Update</button>
+    <form @submit.prevent="updateContent()">
+      <input
+        placeholder="Set a new tittle"
+        v-model="tittle"
+        type="text"
+        name="tittle"
+         autocomplete="off"
+      />
+      <br />
+      <input
+        placeholder="Set a new description"
+        v-model="subTittle"
+        type="text"
+        name="subTittle"
+         autocomplete="off"
+      />
+      <br /><br />
+      <label for="files" class="btn">Select new Image</label>
+  <input id="files" style="visibility:hidden;" ref="file" type="file" name="image">
+      <br /><br />
+      <button align="center">Save changes</button>
+    <loading v-if="loading"/>
     </form>
+
   </div>
-  <img :src="image" alt="">
+  <!-- <img :src="'data:image/jpg;base64,' + image" alt="" /> -->
 </template>
 
 <script>
 import axios from "axios";
+import loading from '@/components/loading.vue'
 export default {
+  components:{
+    loading
+  },
   data() {
     return {
       tittle: "",
       subTittle: "",
       image: "",
+      loading:false
     };
   },
   methods: {
-    async datas() {
+    async updateContent() {
+      this.loading = true
       let fd = new FormData();
 
       fd.append("tittle", this.tittle);
       fd.append("subTittle", this.subTittle);
       fd.append("image", this.$refs.file.files[0]);
-      await axios.post("http://localhost:8081/test-files", fd,{
-         headers: {
-      'Content-Type': 'multipart/form-data'
-    }
-      });
+      await axios.post("http://localhost:8081/landing-page/update", fd, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }).then(function(){this.loading = false}.bind(this));
     },
-  },created(){
-  axios.get("http://localhost:8081/test-files").then(function(res)  {
-    console.log(res);
-
-  //   let base64String = btoa(
-  //           String.fromCharCode.apply(null, new Uint8Array(res.data.items[0].img.data.data))
-  //         );
-   this.image =  res.data;
-    
-    }.bind(this));
-  }
+  },
 };
 </script>
 
 
 <style lang="scss" scoped>
+.landing-form {
+  font-family:$font1 ;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin: 5% 0;
+  form {
+    padding: 20px;
+    width: 340px;
+    text-align: center;
+    input[type="text"] {
+      padding: 6px 16px;
+      width: 250px;
+      font-size: 18px;
+      margin: 15px;
+      border-top: none;
+      border-left: none;
+      border-right: none;
+      background-color: none;
+      text-align:center;
+      &:focus{
+    outline: none;
+      }
+    }
+    label {
+      padding:  13px;
+      font-size: 14px;
+      margin: 15px;
+      border: 1px dotted;
+
+    }
+    button {
+      padding: 12px 16px;
+      text-align: center;
+      border: none;
+      cursor: pointer;
+      background-color: $iconHover;
+      color: white;
+      font-weight:800;
+
+    }
+  }
+}
 </style>
